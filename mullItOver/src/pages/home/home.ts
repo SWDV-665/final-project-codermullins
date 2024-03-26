9f97bb3b2c3b234fa98d19d058fb6e4ad2a2293a
Chris Mullins <mrchrismullins@gmail.com>
2024-03-26 10:03:51 -0400

    added cards and interpolation to home

--- mullItOver/src/pages/home/home.ts
+++ mullItOver/src/pages/home/home.ts
@@ -7,6 +7,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  title = "Review List";
+
+  reviews = [
+    {
+      img: "md-image",
+      name: "McDonalds",
+      thoughts: "food tastes like cardboard",
+      rating: "star"
+    },
+    {
+      img: "md-image",
+      name: "Burger King",
+      thoughts: "food has more flavor",
+      rating: "star"
+    },
+    {
+      img: "md-image",
+      name: "Taco Bell",
+      thoughts: "Cheap food that you can eat one handed",
+      rating: "star"
+    },
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
